875d996ac565f37cdf44a7886b08fce6419e7b95
Eckaos <nathan.pollart@hotmail.fr>
2022-02-24 18:00:12 -0500

    Update position.js

--- ressource/javascript/position.js
+++ ressource/javascript/position.js
@@ -1,7 +1,7 @@
 function populateMapGrid(x, y, playersCoordinate) {
     let mapGrid = document.querySelector(".map-grid");
     mapGrid.innerHTML = ""
-    console.log(x, y);
+    console.log(x, y, playersCoordinate);
     for (let i = 0; i < 10; i++)
         for(let j = 0; j < 10; j++)
             mapGrid.appendChild(createElement(x+j, y+i, playersCoordinate));
