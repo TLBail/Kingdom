45f927700aa7835e9e017f34d42c5f5f310a5e49
Eckaos <nathan.pollart@hotmail.fr>
2022-03-02 16:52:17 -0500

    Update position.js

--- ressource/javascript/position.js
+++ ressource/javascript/position.js
@@ -76,7 +76,7 @@ document.body.addEventListener("changePage", ()=>{
     let cellSize = 40
 
     canvas.addEventListener("click", event => test(Math.floor(event.offsetX/cellSize),Math.floor(event.offsetY/cellSize)))
-    canvas.addEventListener("mouseover", event => test2(Math.floor(event.offsetX/cellSize), Math.floot(event.offsetY.cellSize), ctx))
+    canvas.addEventListener("mouseover", event => test2(Math.floor(event.offsetX/cellSize), Math.floor(event.offsetY.cellSize), ctx))
     drawGrid(canvas, ctx, cellSize)
     getPlayerCoordinateList(ctx, cellSize);
     /*document.querySelector(".top-btn").addEventListener("click", ()=>{
