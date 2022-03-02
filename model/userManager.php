115bb142e577d4710c4da69388a107281c0d9f51
Eckaos <nathan.pollart@hotmail.fr>
2022-03-02 16:05:19 -0500

    Update userManager.php

--- model/userManager.php
+++ model/userManager.php
@@ -27,9 +27,9 @@ function creationCompte()
         echo "<h1> ERROR L'utilisateur existe déjà !! </h1>";
     } else {
         $position = createPosition()
-        $sql = "INSERT INTO `USER` (`id`, `username`, `password`, `lastTimeOnline`, `position`) VALUES (NULL, ?, ?, current_timestamp(), $position) ";
+        $sql = "INSERT INTO `USER` (`id`, `username`, `password`, `lastTimeOnline`, `position`) VALUES (NULL, ?, ?, current_timestamp(), ?) ";
         $preparedSql = getBDD()->prepare($sql);
-        $preparedSql->execute(array($username, $password));
+        $preparedSql->execute(array($username, $password, $position));
 
         echo "compte créer !";
     }
