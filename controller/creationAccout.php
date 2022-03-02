ebc926b700c2891dd8a0f113838ed03ee55ce382
TLBail <toptoletirand@gmail.com>
2022-03-02 05:29:32 +0100

    #7 meilleur detection

--- controller/creationAccout.php
+++ controller/creationAccout.php
@@ -15,7 +15,7 @@ include("../model/userManager.php");
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../ressource/style.css">
+    <link rel="stylesheet" href="../style/dist/loginPage.css">
 </head>
 
 <body>
--- model/userManager.php
+++ model/userManager.php
@@ -21,7 +21,7 @@ function creationCompte()
     $username = preg_replace("/[^a-zA-Z0-9 ]+/", "", $_POST['newusername']);
     $password = md5($_POST['newpassword']);
 
-    $user = connect($username, $password);
+    $user = utilisateurExisteDeja($username);
 
     if (isset($user)) {
         echo "<h1> ERROR L'utilisateur existe déjà !! </h1>";
@@ -33,6 +33,40 @@ function creationCompte()
         echo "compte créer !";
     }
 }
+
+function utilisateurExisteDeja($username)
+{
+
+    $path = explode("/projet", __DIR__)[0] . "/projet";
+    $path .= "/model/user.class.php";
+    include_once($path);
+
+    $path = explode("/projet", __DIR__)[0] . "/projet";
+    $path .= "/model/bddManager.php";
+    include_once($path);
+
+    $sql = "SELECT *  FROM `USER` WHERE username=? ";
+
+    $query = getBDD()->prepare($sql);
+    $query->execute(array($username));
+
+
+    $index = 1;
+    foreach ($query as $ligne) {
+        $useros[$index] = new User(
+            $ligne['id'],
+            $ligne['username'],
+            $ligne['lastTimeOnline'],
+            $ligne['position']
+
+        );
+        $index = $index + 1;
+    }
+    if (isset($useros[1])) {
+        return $useros[1];
+    }
+}
+
 
 function connexion()
 {
