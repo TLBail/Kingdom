abd11e2956b77a4063061206b913c5e8e2bb82cb
Eckaos <nathan.pollart@hotmail.fr>
2022-03-03 17:29:49 -0500

    Update position.js

--- ressource/javascript/position.js
+++ ressource/javascript/position.js
@@ -1,74 +1,152 @@
+//!GLOBAL VARIABLES
+var canvas
+var ctx
+var cellSize
+
 var playersCoordinate = []
 var players = []
-var x
-var y
-var offset
+
+
+var offsetX
+var offsetY
+var boxNumber
+
+//TODO refaire toute la partie coordonnée
+
+document.body.addEventListener("changePage", ()=>{
+
+    initGlobalValues()
+
+    canvas.addEventListener("click", event => expedition(calculateEventXPosOnGrid(event.offsetX), calculateEventYPosOnGrid(event.offsetY)))
+    canvas.addEventListener("pointermove", event => tooltip(calculateEventXPosOnGrid(event.offsetX), calculateEventYPosOnGrid(event.offsetY)))
+
+    getPlayerCoordinateList()
+
+    document.querySelector(".top-btn").addEventListener("click", ()=>{
+        offsetY--
+        getPlayerCoordinateList()
+    })
+    document.querySelector(".left-btn").addEventListener("click", ()=>{
+        offsetX--
+        getPlayerCoordinateList()
+    })
+    document.querySelector(".right-btn").addEventListener("click", ()=>{
+        offsetX++
+        getPlayerCoordinateList()
+    })
+    document.querySelector(".down-btn").addEventListener("click", ()=>{
+        offsetY++
+        getPlayerCoordinateList()
+    })
+}, false)
+
+
+
+
+function initGlobalValues(size) {
+    canvas = document.querySelector("#map")
+    canvas.width = size
+    canvas.height = size
+    ctx = canvas.getContext("2d")
+    cellSize = 40
+    boxNumber = size/cellSize
+    offsetX = 0
+    offsetY = 0
+}
+
 
 function expedition(x, y) {
-    let i =playersCoordinate.findIndex((t) => isEqual(t,{"x":x,"y":y}));
+    let i =playersCoordinate.findIndex((t) => isEqual(t,{"x":calculateMapPosXtoStoredPosX(x),"y":calculateMapPosYtoStoredPosY(y)}));
     if(i != -1)
         console.log(players[i])
     //TODO link au expedition
 }
 
 
-function tooltip(coordX,coordY, canvas, ctx, cellSize) {
-    let i =playersCoordinate.findIndex((t) => isEqual(t,{"x":coordX-x,"y":coordY-y}));
-    console.log(players, playersCoordinate,i, players[i], coordX, coordX-x, coordY, coordY-y)
-    if(i != -1){
-        redraw(canvas, ctx, cellSize)
-        ctx.fillStyle = "black"
-        ctx.font = "12px serif"
-        ctx.fillText(players[i], (coordX-x*cellSize+cellSize/2), (coordY-y*cellSize+12))
-    }else{
-        redraw(canvas, ctx, cellSize)
-    }
+function tooltip(coordX, coordY) {
+    let i =playersCoordinate.findIndex((t) => isEqual(t,{"x":calculateMapPosXtoStoredPosX(coordX),"y":calculateMapPosYtoStoredPosY(coordY)}));
+    if(i != -1) drawTooltip(players[i], coordX, coordY)
 }
 
-function redraw(canvas, ctx, cellSize) {
-    drawGrid(canvas, ctx,cellSize)
-    for (const coord of playersCoordinate) {
-        if(coord.x>= x && coord.x<x+offset && coord.y>=y && coord.y<y+offset){
-            drawPresence(ctx, cellSize, coord.x-x, coord.y-y)
-        }
-    }
-}
-
-function getPlayerCoordinateList(canvas, ctx, cellSize){
+//!AJAX FUNCTIONS
+function getPlayerCoordinateList(){
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = ()=>{
         if(xhr.readyState == 4 && xhr.status == 200)
-            processResponse(xhr.responseText,canvas, ctx, cellSize)
+            processResponse(xhr.responseText)
     }
-    xhr.open("GET", "./controller/map.php?")
+    xhr.open("GET", './controller/map.php?bound="'+offsetX*boxNumber+','+offsetY*boxNumber+','+boxNumber+'"')//TODO refaire map.php pour ne renvoyer que les player qui sont dans l'interval
     xhr.send(null)
 }
 
-
-
-function processResponse(response, canvas, ctx, cellSize){
+function processResponse(response){
+    players = []
+    playersCoordinate = []
     const playerListJSON = JSON.parse(response);
-    Object.keys(playerListJSON).forEach((key, index)=>{
-        let position = JSON.parse(playerListJSON[key]['position'])
-        let player = playerListJSON[key]['username']
+    parseResponse(playerListJSON)
+    drawAllPresence()
+}
+
+//! UTILITIES FUNCTIONS
+function calculateCenteredPosXOnCanvas(x) {
+    return (x)*(cellSize)+(cellSize/2)
+}
+
+function calculateCenteredPosYOnCanvas(y) {
+    return (y)*(cellSize/2)+(cellSize/2)
+}
+function calculateEventXPosOnGrid(x) {
+    Math.floor(x/cellSize)
+}
+
+function calculateEventYPosOnGrid(y) {
+    return Math.floor(y/cellSize)
+}
+
+function calculateStoredPosXToMapPosX(x) {
+    return x-offsetX*boxNumber
+}
+
+function calculateStoredPosYtoMapPosY(y) {
+    return y-offsetY*boxNumber
+}
+
+function calculateMapPosXtoStoredPosX(x) {
+    return x+offsetX*boxNumber
+}
+
+function calculateMapPosYtoStoredPosY(y) {
+    return y+offsetY*boxNumber
+}
+
+function parseResponse(response) {
+    Object.keys(response).forEach((key)=>{
+        let position = JSON.parse(response[key]['position'])
+        let player = response[key]['username']
         playersCoordinate.push(position) 
         players.push(player)
     })
-    drawGrid(canvas, ctx, cellSize)
-    for (const coord of playersCoordinate) {
-        if(coord.x>= x && coord.x<x+offset && coord.y>=y && coord.y<y+offset)
-            drawPresence(ctx, cellSize, coord.x-x, coord.y-y)
-    }
 }
 
 const isEqual = (first, second)=>{
     return JSON.stringify(first) === JSON.stringify(second)
 }
 
+//!DRAW FUNCTIONS
+function drawAllPresence() {
+    resetCanvas()
+    drawGrid()
+    for (const coord of playersCoordinate) {
+        drawPresence(calculateStoredPosXToMapPosX(coord.x), calculateStoredPosYtoMapPosY(coord.y))
+    }
+}
 
-function drawGrid(canvas, ctx, cellSize) {
+function resetCanvas() {
     ctx.fillStyle = 'white'
     ctx.fillRect(0,0,canvas.width, canvas.height)
+}
+
+function drawGrid() {
     for (let i = 0; i <= canvas.width; i+=cellSize) {
         ctx.moveTo(i,0)
         ctx.lineTo(i,canvas.height)
@@ -79,51 +157,15 @@ function drawGrid(canvas, ctx, cellSize) {
     ctx.stroke()
 }
 
-function drawPresence(ctx, cellSize, x, y) {
+function drawPresence(x, y) {
     ctx.font = '12px serif'
     ctx.fillStyle = "black"
     ctx.textAlign = 'center'
-    ctx.fillText("here", x*cellSize+cellSize/2, y*cellSize+cellSize/2+6, cellSize);
+    ctx.fillText("here", calculateCenteredPosXOnCanvas(x), calculateCenteredPosYOnCanvas(y)+6, cellSize);
 }
 
-
-document.body.addEventListener("changePage", ()=>{
-    let canvas = document.querySelector("#map")
-    canvas.width = 600
-    canvas.height = 600
-
-    let ctx = canvas.getContext("2d")
-    let cellSize = 40
-
-    canvas.addEventListener("click", event => expedition(Math.floor(event.offsetX/cellSize),Math.floor(event.offsetY/cellSize)))
-    canvas.addEventListener("pointermove", event => tooltip(Math.floor(event.offsetX/cellSize), Math.floor(event.offsetY/cellSize), canvas,ctx,cellSize))
-
-    x = 0
-    y = 0
-
-    getPlayerCoordinateList(canvas, ctx, cellSize, x, y)
-
-    offset = canvas.width/cellSize
-
-    document.querySelector(".top-btn").addEventListener("click", ()=>{
-        x-=offset
-        getPlayerCoordinateList(canvas, ctx, cellSize)
-    })
-    document.querySelector(".left-btn").addEventListener("click", ()=>{
-        y-=offset
-        getPlayerCoordinateList(canvas, ctx, cellSize)
-    })
-    document.querySelector(".right-btn").addEventListener("click", ()=>{
-        y+=offset
-        getPlayerCoordinateList(canvas, ctx, cellSize)
-    })
-    document.querySelector(".down-btn").addEventListener("click", ()=>{
-        x+=offset
-        getPlayerCoordinateList(canvas, ctx, cellSize)
-    })
-}, false)
-
-
-
-
-
+function drawTooltip(playerUsername, x, y) {
+    ctx.fillStyle = "black"
+    ctx.font = "12px serif"
+    ctx.fillText(playerUsername, calculateCenteredPosXOnCanvas(x), calculateCenteredPosYOnCanvas(y)-12)
+}
