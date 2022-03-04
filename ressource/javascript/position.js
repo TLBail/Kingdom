01ab80690ef54f72c31535a84eb785db9f935dc7
Eckaos <nathan.pollart@hotmail.fr>
2022-03-04 12:11:50 -0500

    Update position.js

--- ressource/javascript/position.js
+++ ressource/javascript/position.js
@@ -179,5 +179,5 @@ function drawPresence(x, y) {
 function drawTooltip(playerUsername, x, y) {
     ctx.fillStyle = "black"
     ctx.font = "18px serif"
-    ctx.fillText(playerUsername, calculateCenteredPosXOnCanvas(x), calculateCenteredPosYOnCanvas(y)-20 < 0 ? calculateCenteredPosYOnCanvas(y)+20 : calculateCenteredPosYOnCanvas(y)-20)
+    ctx.fillText(playerUsername, calculateCenteredPosXOnCanvas(x), calculateCenteredPosYOnCanvas(y)-20 <= 0 ? calculateCenteredPosYOnCanvas(y)+40 : calculateCenteredPosYOnCanvas(y)-20)
 }
